2e93adc539588b7c32e0cc923f9f9899358ea066
Denis Rudov <denis.rudov@gmail.com>
2019-01-07 12:56:23 +0300

    parseInt

--- string.go
+++ string.go
@@ -1,8 +1,28 @@
 package go_helpers
 
-import "strings"
+import (
+	"errors"
+	"regexp"
+	"strconv"
+	"strings"
+)
 
 // Removes more than one space from a string
 func StringNoSpaces(s string) string {
 	return strings.Join(strings.Fields(s), " ")
+}
+
+// Parses string and found first
+// occurrence of an integer in a given string
+// "some string 3000 4000 some other text " == int(3000)
+func ParseInt(s string) (ret int, err error) {
+	ret = 0
+	compile := regexp.MustCompile(`(\d+)`)
+	submatch := compile.FindStringSubmatch(s)
+	if submatch != nil && len(submatch) > 0 {
+		ret, err = strconv.Atoi(submatch[0])
+		return
+	}
+	err = errors.New("no integer found")
+	return
 }
--- string_test.go
+++ string_test.go
@@ -1,12 +1,33 @@
 package go_helpers
 
 import (
-	"github.com/magiconair/properties/assert"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestStringNoSpaces(t *testing.T) {
 
 	assert.Equal(t, StringNoSpaces(`some  some 1      some`), "some some 1 some")
+
+}
+
+func TestParseInt(t *testing.T) {
+
+	tests := []string{
+		"3000 some string",
+		"some 3000 some string",
+		"some 3000 4000 some string",
+		"some 3000.42 4000 some string",
+	}
+
+	for _, str := range tests {
+		ret, err := ParseInt(str)
+		assert.NoError(t, err)
+		assert.Equal(t, ret, 3000)
+	}
+
+	ret, err := ParseInt("some no integer string")
+	assert.Error(t, err)
+	assert.Equal(t, ret, 0)
 
 }
